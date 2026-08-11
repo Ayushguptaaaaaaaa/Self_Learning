8909c5fbc1dd79b9f970b8b4fff52e5aa7a1e885
Ayush Gupta <ayushgupta7170@gmail.com>
2026-08-11 13:57:24 +0530

    ChatBOT

---
+++ Ai_ChatBot/main.py
@@ -0,0 +1,38 @@
+import openai, os
+from openai import OpenAI
+
+key = os.environ.get("OPENAI_API_KEY")
+
+messages = []
+
+client = OpenAI(
+    api_key=key,
+)
+
+def completion(message):
+    global messages
+    messages.append(
+        {
+            "role": "user",
+            "content": message
+        }
+    )
+
+    chat_completion = client.chat.completions.create( messages=messages,
+                        model="gpt-4o"
+                        )
+    
+    # print(chat_completion)
+    message = {
+        "role": "assistant",
+        "content": chat_completion.choices[0].message.content
+    }
+    messages.append(message)
+    print(f"Jarvis: {message["content"]}")
+
+if __name__ == "__main__":
+    print(f"Jarvis: Hi I am Jarvis, How may I help you\n")
+    while True:
+        user_question = input()
+        print(f"User: {user_question}")
+        completion(user_question)
